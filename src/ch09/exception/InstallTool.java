454083cc4dc8887c0e42152b82fed6fcbcdae28c
itsme <itsme@andy-gram>
2018-01-16 06:46:08 +0900

    ..

---
+++ src/ch09/exception/InstallTool.java
@@ -0,0 +1,18 @@
+package ch09.exception;
+
+public class InstallTool {
+	void copy() {
+		System.out.println("파일 복사");
+	}
+
+	void install() throws Exception {
+		System.out.println("설치");
+		if (Math.random() > 0.5) {
+			throw new Exception("설치 과정에서 문제 발생");
+		}
+	}
+
+	void delete() {
+		System.out.println("파일 삭제");
+	}
+}
