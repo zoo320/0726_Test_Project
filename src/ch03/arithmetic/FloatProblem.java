3e00e1bc50323b907bfeb4ef48016bd8cde8857c
itsme <itsme@DESKTOP-U054VQN>
2019-07-27 13:18:11 +0900

    ..

--- src/ch03/arithmetic/FloatProblem.java
+++ src/ch03/arithmetic/FloatProblem.java
@@ -11,12 +11,12 @@ public class FloatProblem {
 		double d1 = 2.0;
 		double d2 = 1.1;
 		System.out.println("double 연산 결과: " + (d1 - d2));
-		
+
 		int i1 = (int)(d1 * 10);
 		int i2 = (int)(d2 * 10);
 		double result = (i1 - i2)/10.0;
 		System.out.println("변환 후 처리: " + result);  // 0.9
-		
+
 		BigDecimal bd1 = new BigDecimal("2.0");
 		BigDecimal bd2 = new BigDecimal("1.1");
 		System.out.println(bd1.subtract(bd2));
